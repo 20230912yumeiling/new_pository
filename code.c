3552b8615f9c0e6603502e50c0d5da28b932d9c9
20230912yumeiling <3331403039@qq.com>
2024-06-07 20:09:14 +0800

    Update code.c

--- code.c
+++ code.c
@@ -1,11 +1,5 @@
 #include<stdio.h>
 int main(void){
-	printf("test code1\n");
-	
-	printf("test code2\n");
-	printf("test code3\n");
-	printf("test code4\n");
-	printf("test code5\n");
-	printf("test code6\n");
+	printf("删库跑路，hhh\n"）；
 	return ;
 }
